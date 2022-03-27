dc12235e3a103efcaff8b9cbf7106f04e85952bf
Chaitanya Nawale <chaitanya6.nawale@gmail.com>
2022-03-27 16:03:36 +0530

    Added CSES problem set submissions

---
+++ CSES/1068_WeirdAlgorithm.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    long long n;
+    cin>>n;
+    while(true) {
+        cout<<n<<endl;
+        if(n==1) break;
+        if(n&1){
+            n = n*3 +1;
+        }
+        else {
+            n /=2;
+        }
+    }
+    return 0;
+}
