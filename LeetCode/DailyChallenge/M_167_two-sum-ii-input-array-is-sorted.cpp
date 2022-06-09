876e500287c3a35e73ffee1b93d8bad0537671a0
Chaitanya Nawale <chaitanya6.nawale@gmail.com>
2022-06-09 13:16:32 +0530

    LeetCode DailyChallenge 09-Jun-2022

---
+++ LeetCode/DailyChallenge/M_167_two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i = 0;
+        int k = numbers.size()-1;
+        while(i!=k){
+            if(numbers[i]+numbers[k] == target){
+                vector<int> result;
+                result.push_back(i+1);
+                result.push_back(k+1);
+                return result;
+            }
+            else if( numbers[i] + numbers[k] < target ) {
+                i++;
+            }
+            else {
+                k--;
+            }
+        }
+        return numbers;
+    }
+};
